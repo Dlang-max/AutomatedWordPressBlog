6569089b7a02bb91ecb87a6499d444cf19445f04
Dlang-max <langd052405@gmail.com>
2023-07-12 17:04:03 -0400

    got login and signup page working. now I have to update userdr data when they create an account

--- Automated-Blog-v2.0/app.py
+++ Automated-Blog-v2.0/app.py
@@ -8,13 +8,21 @@ CORS(app)
 @app.route('/process-form', methods=['POST'])
 def process_form():
     if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        
+        topic = request.form.get('topic')
+        title = request.form.get('title')
+        links = request.form.get('links')
+        keywords_and_phrases = request.form.get('keywords-and-phrases')
+        preview_image = request.form.get('preview-image')
+
         # Process the form data as needed (e.g., store in a database)
-        
+        print(topic)
+        print(title)
+        print(links)
+        print(keywords_and_phrases)
+        print(preview_image)
+
+        response_data = { 'response': 'Form data received successfully.' }
         # Send a response back to the client
-        response_data = {'name': name, 'email': email, 'response': 'Hello ' + name + '!'}
         return jsonify(response_data)
     else:
         return "Method Not Allowed", 405
---
+++ Automated-Blog-v3.0/website/models.py
@@ -0,0 +1,14 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    website_url = db.Column(db.String(150))
+    website_application_password = db.Column(db.String(150))
+    membership_level = db.Column(db.String(150))
+    has_paid = db.Column(db.Boolean)
