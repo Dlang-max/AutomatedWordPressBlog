1faf7cb78f1def9bdd89c199f5a16873857e0709
Dlang-max <langd052405@gmail.com>
2023-07-13 23:36:01 -0400

    working on styling page. need to understand flex box better

--- Automated-Blog-v3.0/website/views.py
+++ Automated-Blog-v3.0/website/views.py
@@ -9,4 +9,4 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    return render_template("base.html", user=current_user)
