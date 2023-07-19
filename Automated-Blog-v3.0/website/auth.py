e71ed79f3d80f1c730e73278e64b417950f28da7
Dlang-max <langd052405@gmail.com>
2023-07-19 19:33:15 -0400

    working on membership selection page

--- Automated-Blog-v3.0/website/auth.py
+++ Automated-Blog-v3.0/website/auth.py
@@ -78,3 +78,9 @@ def sign_up():
             return redirect(url_for('views.dashboard'))
 
     return render_template("sign_up.html", user=current_user)
+    
+@auth.route('/upgradeMembership', methods=['GET', 'POST'])
+@login_required
+def upgradeMembership():
+    
+    return render_template("upgradeMembership.html", user=current_user)
